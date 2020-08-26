4afddf4a0b58a4a09de3d17bb83acfe19144a2b8
SYW23 <35321639+SYW23@users.noreply.github.com>
2020-08-26 18:04:54 +0800

    Update Player.py

--- klasses/Player.py
+++ klasses/Player.py
@@ -1,20 +1,34 @@
 import sys
 sys.path.append('../')
-from util import minusMinutes, LoadPickle
+from util import LoadPickle
+from klasses.stats_items import regular_items, playoff_items
 
 class Player():
     def __init__(self, pm, ROP):    # 构造参数：球员唯一识别号，常规赛or季后赛
-        self.playerFileDir = './data/players/' + pm + '/%sGames/%sGameBasicStat.pickle' % (ROP, ROP)
+        self.pm = pm
+        self.playerFileDir = 'D:/sunyiwu/stat/data/players/' + pm + '/%sGames/%sGameBasicStat.pickle' % (ROP, ROP)
         self.ROP = ROP
         self.games = LoadPickle(self.playerFileDir)
         self.seasons = len(self.games)\
                           if ROP == 'regular'\
                           else len(self.games) - 1
     
-    def yieldSeasons(self):
+    def yieldSeasons(self, pt=True):
         for season in range(self.seasons):
-            yield self.games[season] if self.ROP == 'regular'\
-                                           else self.games[season + 1]
+            s = self.games[season] if self.ROP == 'regular'\
+                                   else self.games[season + 1]
+            if s or (len(s) == 1 and s == ['G', 'Date', 'Age', 'Tm', '', 'Opp',
+                                           '', 'GS', 'MP', 'FG', 'FGA', 'FG%',
+                                           '3P', '3PA', '3P%', 'FT', 'FTA',
+                                           'FT%', 'ORB', 'DRB', 'TRB', 'AST',
+                                           'STL', 'BLK', 'TOV', 'PF', 'PTS',
+                                           'GmSc', '+/-']):
+                if pt:
+                    if self.ROP != 'regular':
+                        print('season %s' % s[0][0][1][:4])
+                    else:
+                        print('season %s' % s[1][1][:4])
+                yield s
     
     def yieldGames(self, season):
         '''
@@ -40,3 +54,64 @@ class Player():
                 for s in season:
                     for g in s:
                         yield g
+    
+    def seasonAVE(self, ind, item, ROP):
+        # 求取赛季平均，传入参数：赛季序号、统计项名称、常规赛or季后赛
+        s = 0
+        seas = self.games[ind][1:]
+        i = regular_items[item] if ROP == 'regular' else playoff_items[item]
+        for g in seas:
+            s += int(g[i])
+        return s / len(seas)
+    
+    def searchGame(self, comboboxs, stats):
+        # 单场比赛数据查询
+        flag = 0
+        for i in stats:
+            if i.get():    # 未设置查询条件
+                flag = 1
+                break
+        if not flag:    # 未设置查询条件，返回[-1]
+            return [-1]
+        RP = 1 if self.ROP == 'regular' else 0
+        resL = []
+        for s in self.yieldSeasons(pt=False):
+            for game in self.yieldGames(s):
+                res = 1
+                for i, item in enumerate(game + ['']):
+                    if stats[i].get():    # 本项数据统计有设置
+                        if comboboxs[i].get() in ['    >=', '    <=']:    # 数值或字符串比较
+                            if i == 1:    # 日期
+                                x = item[:8]
+                                y = stats[i].get()
+                            elif (RP and (i == 2 or i == 8)) or (not RP and i == 9):    # 年龄或上场时间
+                                x = item[:2] + item[3:]
+                                y = stats[i].get()[:2] + stats[i].get()[3:]
+                            elif (RP and i == 29) or (not RP and i == 30):    # 分差
+                                x = game[6][3:-1] if RP else game[7][3:-1]
+                                y = stats[i].get()
+                            else:
+                                x = item
+                                y = stats[i].get()
+                            if not eval(x + comboboxs[i].get() + y):
+                                res = 0
+                                break
+                        else:    # 字符串相等比较
+                            if i == 4:    # 主客场
+                                x = 0 if item else 1
+                                y = stats[i].get()
+                            elif (RP and i == 6) or (not RP and i == 7):    # 赛果
+                                x = item[0]
+                            else:
+                                x = item
+                            if not eval('\'%s\'%s\'%s\'' % (x, comboboxs[i].get(), stats[i].get())):
+                                res = 0
+                                break
+                if res:    # 符合条件，添加至结果列表
+                    resL.append(game)
+        return resL
+    
+    
+    
+    
+    
